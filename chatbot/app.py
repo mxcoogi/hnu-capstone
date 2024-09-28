6e5e05cf18e27f19cc44899f4c6736d0dd42f04c
mxcoogi <mxcoogi@gmail.com>
2024-09-28 15:10:29 +0900

    function 구현

--- chatbot/app.py
+++ chatbot/app.py
@@ -1,11 +1,16 @@
 from flask import Flask , request
-import sys
 from chatbot import Chatbot
 from common import model
+from function_calling import FunctionCalling, tools 
+from characters import system_role, instruction
 
-hnu = Chatbot(model.basic)
-
+hnu = Chatbot(
+    model = model.basic,
+    system_role = system_role,
+    instruction = instruction
+    )
 app = Flask(__name__)
+func_calling = FunctionCalling(model=model.basic)
 
 @app.route("/chat-api", methods = ['POST'])
 def chat_api():
@@ -21,7 +26,15 @@ def chat_api():
         'department' : department
     }
     hnu.add_user_message(request_message, student_info)
-    response = hnu.send_request()
-    hnu.add_response(response)
+    analyzed, analyzed_dict = func_calling.analyze(request_message, tools)
+    if analyzed_dict.get("tool_calls"):
+        response = func_calling.run(analyzed, analyzed_dict, hnu.context[:])
+        hnu.add_response(response)
+    else:
+        response = hnu.send_request()
+        hnu.add_response(response)
+
     response_message = hnu.get_response_content()
+    hnu.handle_token_limit(response)
+    hnu.clean_context()
     return response_message
--- chatbot/common.py
+++ chatbot/common.py
@@ -1,6 +1,34 @@
 import config
 from openai import OpenAI
 from dataclasses import dataclass
+import pytz
+from datetime import datetime, timedelta
+
+def today():
+    korea = pytz.timezone('Asia/Seoul')
+    now = datetime.now(korea)
+    return (now.strftime("%Y%m%d"))
+
+def current():
+    korea = pytz.timezone('Asia/Seoul')
+    now = datetime.now(korea)
+    formatted_now = now.strftime("%Y.%m.%d %H:%M:%S")
+    return formatted_now
+    
+def makeup_response(message, finish_reason="ERROR"):
+    return {
+                "choices": [
+                    {
+                        "finish_reason": finish_reason,
+                        "index": 0,
+                        "message": {
+                            "role": "assistant",
+                            "content": message
+                        }
+                    }
+                ],
+                "usage": {"total_tokens": 0},
+            }
 
 @dataclass(frozen= True)
 class Model:
